fad9af762ef24bed4f8de0ff175931b088ee4ddc
Joe Heck <heckj@mac.com>
2021-12-16 15:10:05 -0800

    Initial Commit

---
+++ Shared/BenchmarkTrialApp.swift
@@ -0,0 +1,17 @@
+//
+//  BenchmarkTrialApp.swift
+//  Shared
+//
+//  Created by Joseph Heck on 12/16/21.
+//
+
+import SwiftUI
+
+@main
+struct BenchmarkTrialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
